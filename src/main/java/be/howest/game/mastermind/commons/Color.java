fbbe0e236db301ac8e3de0adc367811933a99ea3
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-06-15 21:07:46 +0200

    Color enum - 1.0.b-RELEASE

--- src/main/java/be/howest/game/mastermind/commons/Color.java
+++ src/main/java/be/howest/game/mastermind/commons/Color.java
@@ -2,14 +2,28 @@ package be.howest.game.mastermind.commons;
 
 import java.util.Locale;
 
+/**
+ * Color enumeration for a MasterMind game
+ * @author Hayk
+ *
+ */
 public enum Color {
 	
 	RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, PINK, WHITE, BROWN, BLACK;
 	
+	/**
+	 * Gets the lowercase color name
+	 * @return Lowercase color name
+	 */
 	public String getName() {
 		return this.toString().toLowerCase(Locale.ENGLISH);
 	}
 	
+	/**
+	 * Gets Color enumerated value by a color name
+	 * @param name Color name
+	 * @return null if name is null or if such a color does not exist in the Enumeration, or the Color if it exists
+	 */
 	public static Color get(String name){
 		if (name == null)
 			return null;
